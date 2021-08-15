290e46aad9ece22367b94868241a5736b94fd62c
Andrey Rubtsov <mail@undrey.com>
2021-08-15 17:58:57 +0300

    add html template

--- application.py
+++ application.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask,render_template
+var1 = "Undrey"
+var2 = "Test"
 application = Flask(__name__)
+
+
 
 @application.route("/")
 def root():
-    return "Hello, world"
+    return render_template("index.html")
 
 
 if __name__== "__main__":
+    application.debug= True
     application.run()
