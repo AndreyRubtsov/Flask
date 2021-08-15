9255d8622f48f1204d33fae25a0784e2eff251da
Andrey Rubtsov <mail@undrey.com>
2021-08-16 00:14:43 +0300

    first 50 tracks

--- application.py
+++ application.py
@@ -7,27 +7,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def apple_root():
-    api_url = "https://itunes.apple.com/lookup?id=487143&entity=song"
+    api_url = "https://itunes.apple.com/lookup?id=487143&entity=song&limit=200"
     r = requests.get(api_url).json()
-    print("List lenght:", len(r['results']))
-
-    # pink_floyd_data = {
-    #     "collectionName": r['results'][1]['collectionName'],
-    #     "trackName": r['results'][1]['trackName'],
-    #     "collectionPrice": r['results'][1]['collectionPrice'],
-    #     "trackPrice": r['results'][1]['trackPrice'],
-    #     "primaryGenreName": r['results'][1]['primaryGenreName'],
-    #     "trackCount": r['results'][1]['trackCount'],
-    #     "trackNumber": r['results'][1]['trackNumber'],
-    #     "releaseDate": r['results'][1]['releaseDate'],
-    # }
-    #    print(pink_floyd_data)
 
     del r['results'][0]
     track_array = []
-    i=0
+    i = 0
     for res in r['results']:
-        i+=1
+        i += 1
         pink_floyd_data = {
             "num": i,
             "collectionName": res['collectionName'],
@@ -40,7 +27,6 @@ def apple_root():
             "releaseDate": res['releaseDate'],
         }
         track_array.append(pink_floyd_data)
-    print(track_array)
 
     return render_template("index.html", datas=track_array)
 
